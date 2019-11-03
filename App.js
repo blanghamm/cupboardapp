d305bd5eaa6591c9f3645214950d582820205254
Josh236 <holdaway236@gmail.com>
2019-11-03 22:56:10 +0000

    firebase updated
    added firebase and java

--- App.js
+++ App.js
@@ -4,10 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
 import { ScrollView } from 'react-native-gesture-handler';
 
+import { TextInput } from 'react-native-paper';
+
+import firebase from './firebase';
+
+var database = firebase.database();
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('/users/randomtext' + userId).set({
+    username: a,
+    email: a,
+    profile_picture : a
+  });
+}
+
+console.log(firebase.name);
+console.log(firebase.database());
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: ''
     };
   }
 
@@ -21,7 +39,12 @@ export default class App extends Component {
           <ScrollView>
             <Text style={{ fontSize: 24,fontWeight: '700'}}>
               What on earth
-              </Text>           
+              </Text>
+
+              <TextInput label='Text' value={this.state.text}
+              onChangeText={text => this.setState({ text })}
+              />
+
             </ScrollView>
 
 
