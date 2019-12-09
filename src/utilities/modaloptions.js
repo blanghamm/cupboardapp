ebaf4988a3642c75545efe40b552524a701f02e3
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2019-12-09 18:44:01 +0000

    styling updates - SAK

--- src/utilities/modaloptions.js
+++ src/utilities/modaloptions.js
@@ -5,7 +5,7 @@ import Icon from 'react-native-vector-icons/Feather';
 
 class Modaloptions extends Component {
   state = {
-    isModalVisible: false,
+    isModalVisible: true,
   };
 
   toggleModal = () => {
