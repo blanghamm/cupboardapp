4a4d72bd2b7aa498e6fdba649c046117ab5d2946
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2019-12-09 18:16:43 +0000

    styling updates - SAK

--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Image,
   Button,
+  TouchableOpacity,
 } from 'react-native';
 import {Carousel} from 'react-native-snap-carousel';
 import firebase from '../firebase/config';
@@ -15,6 +16,44 @@ import {signOut} from '../store/actions/authActions';
 import Styles from '../styles';
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carouselItems: [
+        {
+          //id: (FROM DATABASE) ,
+          title: 'Item 1',
+          //thumbnail: '../assets/stock.png',
+        },
+        {
+          title: 'Item 2',
+        },
+        {
+          title: 'Item 3',
+        },
+        {
+          title: 'Item 4',
+        },
+        {
+          title: 'Item 5',
+        },
+      ],
+    };
+  }
+
+  _renderItem({item, index}) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#faf',
+        }}>
+        <Text style={{color: '#000000'}}>{item.title}</Text>
+      </View>
+    );
+  }
   render() {
     return (
       <View>
@@ -29,14 +68,51 @@ class Welcome extends React.Component {
           <Text style={Styles.welcomeTextBlack}>Username</Text>
         </View>
         <View style={Styles.wideBlock}>
-          <Text style={Styles.welcomeTextGrey}>You’re nearly stocked up</Text>
+          <View style={Styles.bannerBlock}>
+            <View style={{width: '70%'}}>
+              <Text style={(Styles.bodyText, {fontSize: 24})}>
+                You’re nearly stocked up msg//
+              </Text>
+            </View>
+            <View style={{width: '30%'}}>
+              <Text style={(Styles.bodyText, {fontSize: 24})}>graph// </Text>
+            </View>
+          </View>
         </View>
-        <View>
-          <Button
-            title="Go Back"
-            onPress={() => this.props.navigation.navigate('Loading')}
-          />
+        <View style={Styles.standardBlock}>
+          <Text style={Styles.bodyTitle}>Ready to shop?</Text>
+          <Text style={Styles.bodyText}>Your nearest shop is //DATA//</Text>
+          <View style={Styles.halfButtonBlock}>
+            <TouchableOpacity
+              style={[Styles.halfButton, Styles.greyButton]}
+              onPress={this.onPress}>
+              <Text style={Styles.buttonText}>View map</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[Styles.halfButton, Styles.peachButton]}
+              onPress={this.onPress}>
+              <Text style={Styles.buttonText}>View map</Text>
+            </TouchableOpacity>
+          </View>
         </View>
+        <View style={Styles.standardBlock}>
+          <Text style={Styles.bodyTitle}>Your Recipes</Text>
+          <TouchableOpacity
+            style={[Styles.fullButton, Styles.greyButton]}
+            onPress={this.onPress}>
+            <Text style={Styles.buttonText}>View map</Text>
+          </TouchableOpacity>
+        </View>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.something}>
+            <Carousel
+              data={this.state.carouselItems}
+              sliderWidth={250}
+              itemWidth={250}
+              renderItem={this._renderItem}
+            />
+          </View>
+        </SafeAreaView>
       </View>
     );
   }
--- src/styles/index.js
+++ src/styles/index.js
@@ -5,6 +5,7 @@ import Layout from './layout';
 import Typography from './typography';
 
 export default StyleSheet.create({
+  //LAYOUTS
   centerElement: {
     alignItems: 'center',
   },
@@ -22,15 +23,74 @@ export default StyleSheet.create({
     borderWidth: 1,
     borderColor: 'grey',
   },
+  bannerBlock: {
+    height: 100,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   tallBlock: {
     marginTop: Layout.tall,
     paddingHorizontal: Layout.standard,
   },
+  halfButtonBlock: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  //BUTTONS
+
+  fullButton: {
+    height: 40,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+
+  halfButton: {
+    width: 150,
+    height: 40,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  greyButton: {
+    backgroundColor: Colors.grey,
+  },
+
+  peachButton: {
+    backgroundColor: Colors.peach,
+  },
+
+  aquaButton: {
+    backgroundColor: Colors.aqua,
+  },
+
+  lavenderButton: {
+    backgroundColor: Colors.lavender,
+  },
+
+  buttonText: {
+    fontFamily: Typography.bodyRegular,
+    textTransform: 'uppercase',
+    fontSize: 12,
+    color: Colors.white,
+  },
+
+  //ICONS
+
   largeIcon: {
     tintColor: Colors.black,
     width: 50,
     height: 50,
   },
+
+  //TEXT
+
   welcomeTextGrey: {
     color: Colors.grey,
     fontSize: 40,
@@ -41,5 +101,20 @@ export default StyleSheet.create({
     fontSize: 40,
     fontFamily: Typography.titleRegular,
     lineHeight: 40,
+  },
+  bodyText: {
+    color: Colors.black,
+    fontFamily: Typography.bodyRegular,
+  },
+  bodyTitle: {
+    color: Colors.black,
+    fontFamily: Typography.bodyBold,
+    textTransform: 'uppercase',
+    fontSize: 20,
+  },
+  bodySubTitle: {
+    color: Colors.grey,
+    fontFamily: Typography.bodyRegular,
+    fontSize: 16,
   },
 });
