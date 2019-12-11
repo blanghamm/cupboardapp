b052a2d8e779874782adaaf4bb9705bd3993a4dc
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2019-12-11 18:00:37 +0000

    modal - SAK

--- src/screens/cupboard.js
+++ src/screens/cupboard.js
@@ -14,58 +14,11 @@ const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
 const screenWidthMargin = viewportWidth - 60;
 
 export default class cupboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      carouselItems: [
-        {
-          //id: (FROM DATABASE) ,
-          title: 'Item 1',
-          //thumbnail: '../assets/stock.png',
-        },
-        {
-          title: 'Item 2',
-        },
-        {
-          title: 'Item 3',
-        },
-        {
-          title: 'Item 4',
-        },
-        {
-          title: 'Item 5',
-        },
-      ],
-    };
-    console.log(screenWidthMargin);
-  }
-
-  _renderItem({item, index}) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#faf',
-        }}>
-        <Text style={{color: '#000000'}}>{item.title}</Text>
-      </View>
-    );
-  }
-
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.something}>
-          <Carousel
-            data={this.state.carouselItems}
-            sliderWidth={screenWidthMargin}
-            itemWidth={screenWidthMargin}
-            renderItem={this._renderItem}
-          />
-        </View>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <Text style={{color: 'white'}}>CUPBOARD PAGE</Text>
+      </View>
     );
   }
 }
@@ -77,8 +30,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 0,
-  },
-  something: {
-    height: 250,
   },
 });
