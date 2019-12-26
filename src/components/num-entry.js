6ec02ce91aef599aa18e6a9ce3840450c96212b1
Ben Langham <langham1291@outlook.com>
2019-12-26 19:53:16 +0000

    added redux parts for stock - Ben

--- src/components/num-entry.js
+++ src/components/num-entry.js
@@ -15,53 +15,44 @@ import Styles from '../styles';
 import Typography from '../styles/typography';
 import Colors from '../styles/colors';
 import Icon from 'react-native-vector-icons/Feather';
+import {connect} from 'react-redux';
+import {increment, decrement} from '../store/actions/stockActions';
 
-export default class Numentry extends React.Component {
-  state = {
-    ingredient: '',
-    quantVar: 1,
-  };
-
-  minQ = () => {
-    if (this.state.quantVar > 1) {
-      this.setState({
-        quantVar: this.state.quantVar - 1,
-      });
-
-      console.log(this.state.quantVar);
-    }
-  };
-
-  plusQ = () => {
-    this.setState({
-      quantVar: this.state.quantVar + 1,
-    });
-  };
-
-  render() {
-    let quantVar = this.state.quantVar;
-    return (
-      <SafeAreaView
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity onPress={this.minQ}>
-          <Icon style={{}} name="minus" color={Colors.grey} size={30} />
-        </TouchableOpacity>
+const Numentry = ({stocknumber, increment, decrement}) => {
+  return (
+    <SafeAreaView
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+      }}>
+      <TouchableOpacity onPress={decrement}>
+        <Icon style={{}} name="minus" color={Colors.grey} size={30} />
+      </TouchableOpacity>
+      <View>
         <Text
           style={[
             Typography.bodyBold,
             {fontSize: 20, color: Colors.grey, paddingHorizontal: 5},
           ]}>
-          {this.state.quantVar}
+          {stocknumber}
         </Text>
-        <TouchableOpacity onPress={this.plusQ}>
-          <Icon style={{}} name="plus" color={Colors.grey} size={30} />
-        </TouchableOpacity>
-      </SafeAreaView>
-    );
-  }
-}
+      </View>
+      <TouchableOpacity onPress={increment}>
+        <Icon style={{}} name="plus" color={Colors.grey} size={30} />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const mapStateToProps = state => ({
+  stocknumber: state.stocknumber,
+});
+
+const mapDispatchToProps = {
+  increment,
+  decrement,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Numentry);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from './authReducer';
 import {combineReducers} from 'redux';
 import {firebaseReducer} from 'react-redux-firebase';
+import stockReducer from './stockReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   firebase: firebaseReducer,
+  stock: stockReducer,
 });
 
 export default rootReducer;
